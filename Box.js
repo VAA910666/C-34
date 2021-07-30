480fd906120cbb5434ad9db6158e4f00ee399881
VAA910666 <82810873+VAA910666@users.noreply.github.com>
2021-07-30 15:35:11 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,23 @@
+  class Box {
+    constructor(x, y, width, height) {
+      var options = {
+        density:1,
+        restitution:0.8,
+        friction:1
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display() {
+      var pos = this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER)
+      strokeWeight(3)
+      stroke("blue")
+        rect(0,0, this.width, this.height);
+        pop();
+    }
+  };
